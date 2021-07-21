bd523d6d1c233576738197cbb5e94efdbb99f4b4
EwenQuim <ewen.quimerch@gmail.com>
2021-07-21 12:23:40 +0200

    Test ed25519

--- screens/keys/KeysScreen.tsx
+++ screens/keys/KeysScreen.tsx
@@ -3,7 +3,8 @@ import { TabTwoParamList } from "../../types";
 import * as React from "react";
 import { Text, View } from "../../components/Themed";
 import { Button, SafeAreaView, StyleSheet, TextInput } from "react-native";
-import * as ed from "noble-ed25519";
+import * as ed from "./ed";
+import * as Random from "expo-random";
 
 type Props = {
   navigation: StackNavigationProp<TabTwoParamList>;
@@ -13,16 +14,17 @@ export default function KeysManagementScreen({ navigation }: Props) {
   const [text, onChangeText] = React.useState("Useless Text");
 
   const keyGen = async () => {
-    // const privateKey = ed.utils.randomPrivateKey(); // 32-byte Uint8Array or string.
-    // const msgHash =
-    //   "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef";
+    const privateKey = Random.getRandomBytes(32);
+    const msgHash = "deabeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef";
 
-    // const publicKey = await ed.getPublicKey(privateKey);
+    const publicKey = await ed.getPublicKey(privateKey);
+    console.log("pub", publicKey);
     // const signature = await ed.sign(msgHash, privateKey);
     // const isSigned = await ed.verify(signature, msgHash, publicKey);
     // console.log("is signed", isSigned);
 
     console.log("hi");
+    console.log(privateKey);
   };
   return (
     <>
