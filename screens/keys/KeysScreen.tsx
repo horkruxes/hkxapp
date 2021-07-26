11ea9dbd3de3af395cd73e337335fd07e4ffa68d
EwenQuim <ewen.quimerch@gmail.com>
2021-07-26 18:46:08 +0200

    attempt to create keys

--- screens/keys/KeysScreen.tsx
+++ screens/keys/KeysScreen.tsx
@@ -1,28 +1,67 @@
 import { StackNavigationProp } from "@react-navigation/stack";
 import { TabTwoParamList } from "../../types";
 import * as React from "react";
+import { FlatList, StyleSheet } from "react-native";
 import { Text, View } from "../../components/Themed";
-import { Button, SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { TextInput, Button } from "react-native-paper";
 import elliptic, { eddsa as EdDSA } from "elliptic";
 import * as Random from "expo-random";
-import { testing, test2, testNoble, verifyOwnership } from "../../utils/crypto";
+import {
+  genKeyPairAndStore,
+  KeyPairHK,
+  loadKeyPairsFromStorage,
+  testing,
+} from "../../utils/crypto";
+
 type Props = {
   navigation: StackNavigationProp<TabTwoParamList>;
 };
 
 export default function KeysManagementScreen({ navigation }: Props) {
-  const [text, onChangeText] = React.useState("Useless Text");
-  const test = elliptic.utils;
+  const [text, onChangeText] = React.useState("");
+  const [keyPairs, setKeys] = React.useState<KeyPairHK[]>([]);
   const keyGen = async () => {
-    testing();
+    genKeyPairAndStore(text);
+    getKeyPairs();
   };
+
+  const getKeyPairs = async () => {
+    const keyPairs = await loadKeyPairsFromStorage();
+    setKeys(keyPairs);
+  };
+
+  React.useEffect(() => {
+    getKeyPairs();
+  }, []);
+
   return (
     <>
-      <Text>Hi</Text>
-      <TextInput onChangeText={onChangeText} value={text} />
-      <Button title="Hi" onPress={keyGen} />
-      <Button title="Hi 2" onPress={test2} />
-      <Button title="Hi 3" onPress={testNoble} />
+      <View style={styles.newKeyPair}>
+        <TextInput
+          label="name"
+          mode="outlined"
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="Enter a name for your new key pair"
+        />
+        <Button onPress={keyGen}> Add a new Key pair</Button>
+      </View>
+      <FlatList
+        data={keyPairs}
+        keyExtractor={(item) => item.PublicBase64}
+        renderItem={({ item }: { item: KeyPairHK }) => (
+          <>
+            <Text>{item.PrivateBase64}</Text>
+          </>
+        )}
+      />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  main: {},
+  newKeyPair: {
+    padding: 12,
+  },
+});
--- utils/crypto.ts
+++ utils/crypto.ts
@@ -1,17 +1,10 @@
-import elliptic, { eddsa as EdDSA } from "elliptic";
 import { Buffer } from "buffer";
-import { encode, decode } from "base-64";
-import * as ed from "noble-ed25519";
-import { ECDSA } from "@oliverne/easy-ecdsa";
-import b64 from "react-native-base64";
-import edd, { sign, verify } from "@stablelib/ed25519";
+import edd, { sign, verify, generateKeyPair } from "@stablelib/ed25519";
 import { Message } from "../types";
+import * as SecureStore from "expo-secure-store";
+import * as Random from "expo-random";
 
-export function testNoble() {
-  // const privateKey = ed.utils.randomPrivateKey(); // crash
-  // console.log("sec key", privateKey);
-}
-const bytesToBase64 = (bytes: Buffer): string => {
+const bytesToBase64 = (bytes: Uint8Array): string => {
   return Buffer.from(bytes).toString("base64");
 };
 
@@ -41,80 +34,49 @@ export function verifyOwnership(message: Message): boolean {
   return verify(pub, globalMessageToVerify, sig);
 }
 export function testing() {
-  // Create and initialize EdDSA context
-  // (better do it once and reuse it)
-  // // CHECK WITH NO PRIVATE KEY
-  // const msgToVerif = Buffer.from(
-  //   "This is the second post, send from https://horkruxes.amethysts.studio and stored in https://hk.quimerch.com. All posts must be at least 140 characters long (to make sure there is someone behind that is trying to write a proper reflexion, not just spamming unuseful things)",
-  //   "ascii"
-  // );
-  // const pub = base64ToBytes("JL6zyYtrk43MZ+uV7J+y8HFS9MvkI2eZT1RbRnV4Qog=");
-  // const globalMessageToVerify = Buffer.concat([msgToVerif, pub]);
-  // // console.log("Message TOTAL", globalMessageToVerify);
-  // // Import public key
-  // const key2 = ec.keyFromPublic(pub);
-  // console.log("ok");
-  // // Verify signature
-  // const sigBuffer = base64ToBytes(
-  //   "yn8eSEyVUgBMC5d8rFES2d8XAlfDVgYNEu7hmCc/RNjNaut2jBPMEg0IBIasQoYH1r3G2t7m9ifdE+5vDY+OAQ=="
-  // );
-  // console.log("SIG BYTES", sigBuffer);
-  // console.log();
-  // console.log("\n\n\nVERIF", key2.verify(globalMessageToVerify, sigBuffer));
+  const kp = generateKeyPair();
+  console.log("key pair", kp);
 }
 
-export const test2 = () => {
-  const ec = new EdDSA("ed25519");
+export const genKeyPairAndStore = async (name: string) => {
+  console.log("generating...");
 
-  // Create key pair from secret
-  const key = ec.keyFromSecret(
-    base64ToBytes(
-      "KxXJ7x3hbXrju+UvbgK8pWgzHHm8L7+UR7ae7nGBRCAkvrPJi2uTjcxn65Xsn7LwcVL0y+QjZ5lPVFtGdXhCiA=="
-    )
-  ); // hex string, array or Buffer
+  const sec2 = Random.getRandomBytes(32);
+  console.log("random bytes got.");
 
-  console.log("pub key", bytesToBase64(key.getPublic()));
+  const kp = edd.generateKeyPair();
+  console.log("generated.");
 
-  const msg =
-    "This is the second post, send from https://horkruxes.amethysts.studio and stored in https://hk.quimerch.com. All posts must be at least 140 characters long (to make sure there is someone behind that is trying to write a proper reflexion, not just spamming unuseful things)" +
-    "JL6zyYtrk43MZ+uV7J+y8HFS9MvkI2eZT1RbRnV4Qog=";
-  // //
-  const signature = key.sign(msg).toBytes();
-  console.log("sig", bytesToBase64(signature));
+  // const pub2 = edd.extractPublicKeyFromSecretKey(sec2);
+  console.log("extracted");
+
+  const sec = bytesToBase64(kp.secretKey);
+  const pub = bytesToBase64(kp.publicKey);
+  const keyPair = {
+    PrivateBase64: sec,
+    PublicBase64: pub, // Used as id
+    Name: name,
+  };
+
+  const keyPairs = await loadKeyPairsFromStorage();
+  keyPairs.push(keyPair);
+
+  SecureStore.setItemAsync("keyPairs", JSON.stringify(keyPairs));
 };
 
-/**
- * Convert a string to a unicode byte array
- * @param {string} str
- * @return {Array} of bytes
- */
-export function strToUtf8Bytes(str: string) {
-  const utf8 = [];
-  for (let ii = 0; ii < str.length; ii++) {
-    let charCode = str.charCodeAt(ii);
-    if (charCode < 0x80) utf8.push(charCode);
-    else if (charCode < 0x800) {
-      utf8.push(0xc0 | (charCode >> 6), 0x80 | (charCode & 0x3f));
-    } else if (charCode < 0xd800 || charCode >= 0xe000) {
-      utf8.push(
-        0xe0 | (charCode >> 12),
-        0x80 | ((charCode >> 6) & 0x3f),
-        0x80 | (charCode & 0x3f)
-      );
-    } else {
-      ii++;
-      // Surrogate pair:
-      // UTF-16 encodes 0x10000-0x10FFFF by subtracting 0x10000 and
-      // splitting the 20 bits of 0x0-0xFFFFF into two halves
-      charCode =
-        0x10000 + (((charCode & 0x3ff) << 10) | (str.charCodeAt(ii) & 0x3ff));
-      utf8.push(
-        0xf0 | (charCode >> 18),
-        0x80 | ((charCode >> 12) & 0x3f),
-        0x80 | ((charCode >> 6) & 0x3f),
-        0x80 | (charCode & 0x3f)
-      );
-    }
+export const loadKeyPairsFromStorage = async (): Promise<KeyPairHK[]> => {
+  let keyPairs: KeyPairHK[];
+  const keyPairsString = await SecureStore.getItemAsync("keyPairs");
+  if (keyPairsString) {
+    keyPairs = JSON.parse(keyPairsString);
+    return keyPairs;
+  } else {
+    return [];
   }
-  return utf8;
-}
+};
+
+export type KeyPairHK = {
+  PrivateBase64: string;
+  PublicBase64: string; // Used as id
+  Name: string;
+};
